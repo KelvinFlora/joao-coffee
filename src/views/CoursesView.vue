c699fd82289d1d60d7de53737315c7a2be3dee8d
Kelvin Flora <kelvin.flora@gmail.com>
2024-02-17 17:35:44 -0300

    image fix

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -60,7 +60,7 @@
     </div>
     <div>
       <img
-        src="../../public/images/Novidades.jpg"
+        src="/images/Novidades.jpg"
         class="rounded-center align-items-center news-img mt-3 mb-3"
         alt="Xicara de café escrito News Coming Soon com grãos de café em volta"
       />
